0f6d05e3847a2d7f4ca73fc229f48939d08749e8
a503-26 <1>
2018-11-28 14:30:19 +0900

    1128

---
+++ MemoApp/MemoFormVC.swift
@@ -0,0 +1,30 @@
+//
+//  MemoFormVC.swift
+//  MemoApp
+//
+//  Created by 503-26 on 28/11/2018.
+//  Copyright © 2018 memo. All rights reserved.
+//
+
+import UIKit
+
+class MemoFormVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func moveMemo(_ sender: Any) {
+        //하위 뷰 객체 생성
+        let memoListVC = self.storyboard?.instantiateViewController(withIdentifier: "MemoListVC") as? MemoListVC
+        
+        //하위 뷰 타이틀 정하기
+        memoListVC?.navigationItem.title = "메모 목록"
+    
+        //페이지 이동
+        self.navigationController?.pushViewController(memoListVC!, animated: true)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
