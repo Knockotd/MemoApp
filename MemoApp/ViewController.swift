d628f04fe1f98e415c835acab033dd11cf926d5d
a503-26 <1>
2018-11-28 11:09:15 +0900

    Initial Commit

---
+++ MemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 503-26 on 28/11/2018.
+//  Copyright © 2018 memo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
